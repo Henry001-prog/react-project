75923779727f355a80e10fe0e3596cc4248a3c41
Alisson Henrique <alissonhenrique001@hotmail.com.br>
2019-12-06 23:58:46 -0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  StyleSheet,
+  View
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <View style={styles.box} />
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    backgroundColor: 'red',
+  },
+});
